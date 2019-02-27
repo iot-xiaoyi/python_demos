525616bf0564ca796a8ac817b7cdbe50352f1c53
Brown Lee <17602115406@163.com>
2019-02-27 18:57:48 +0800

    add 05-offical

---
+++ 5-offical/flaskr/__init__.py
@@ -0,0 +1,34 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    #应用需要知道在哪里设置路径， 使用 __name__ 是一个方便的方法, 是当前模块的名称
+     # instance_relative_config=True告诉应用配置文件是相对于instance folder的相对路径，
+     # 实例文件夹flaskr包的外面，用于存放本地数据，不应当提交到版本控制系统
+    app = Flask(__name__, instance_relative_config=True)  
+    #设置一个应用的 缺省配置，在开发过程中， 为了方便可以设置SECRET_KEY为 'dev' ，但是在发布的时候应当使用一个随机值来 重载它。
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        #load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        #load the test config if passed in 
+        #test_config 也会被传递给工厂，并且会替代实例配置。这样可以实现 测试和开发的配置分离，相互独立
+        app.config.from_mapping(test_config)
+    
+    #ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def hello():
+        return 'Hello World!'
+
+    return app
---
+++ 5-offical/flaskr/db.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types = sqlite3.PARSE_DECLTYPES
+        )
+        #告诉连接返回类似于字典的行，这样可以通过列名称来操作数据。
+        g.db.row_factory = sqlite3.Row
+    
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
